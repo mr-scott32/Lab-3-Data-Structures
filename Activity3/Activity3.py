ea1634673201ba13511c6a02b4ddcb9bbd81892f
mr-scott32 <128337536+mr-scott32@users.noreply.github.com>
2026-09-25 14:59:44 +1000

    activity 3 added

---
+++ Activity3/Activity3.py
@@ -0,0 +1,7 @@
+"""
+Colesworth now have a .txt file full of items they want to add to their new system. The file is called 'grocery_list.txt'. The system must now:
+- Read the items from the .txt file and add them to a list.
+- Show users what items are available in the list.
+- Users can only add items available on this list to their grocery purchase.
+- When users check out, the users' grocery list should be saved to a new .txt file called 'grocery_checkout.txt'.
+"""
