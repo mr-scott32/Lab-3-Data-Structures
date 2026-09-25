2d5e0cb50d11ce8adcaff92d6be99bf84aff5883
mr-scott32 <128337536+mr-scott32@users.noreply.github.com>
2026-09-25 12:24:10 +1000

    2 activities made

---
+++ Activity1.py
@@ -0,0 +1,12 @@
+"""
+Let's go grocery shopping! Colesworth has requested a shopping application which allows users to do the following:
+- Add a grocery item of their choice to a list (we'll need to start with an empty list)
+- Remove grocery items from the list by entering the name of the grocery item
+- Allow the user to check out (aka finish shopping)
+- The user can continue to do all of the above until they check out.
+
+You do NOT need to handle pricing.
+
+"""
+
+# Program Here
---
+++ Activity2.py
@@ -0,0 +1,14 @@
+"""
+As Year 7 students finish their first semester of school, the Principal has determined that it is difficult 
+for them to track their marks and make determinations as to which subjects may need to be given more priority.
+
+The Principal has commissioned an application which can allow Year 7s to more easily track their marks across
+subjects and make a determination as to which subjects need priority.
+
+The application should:
+- Store all subjects in a dictionary with each having a mark assigned as 0 to start with.
+- Ask the user to enter a mark for every subject.
+- Check every dictionary value and for each under 70, inform the user they need to study more for that subject.
+
+Extension: The second and third requirement can be completed more efficiently with effective iteration (loops).
+"""
